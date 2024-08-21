109efd613324b29f0856cbcd5bb23d2c51d4d329
ross <ross@facere.ai>
2024-08-21 20:44:20 +1000

    add script for downloading models

--- download_models.sh
+++ download_models.sh
@@ -10,6 +10,6 @@ echo "Start downloading models..."
 # 获取token参数
 TOKEN=$1
 # 构建wget命令
-wget -O ./models/checkpoints/architecturerealmix_v11.safetensors "https://civitai.com/api/download/models/431755?&token=${TOKEN}" --content-disposition
-wget -O ./models/loras/mjmimic.safetensors "https://civitai.com/api/download/models/283697?&token=${TOKEN}" --content-disposition
+wget -O ./models/checkpoints/architecturerealmix_v11.safetensors "https://civitai.com/api/download/models/431755?type=Model&format=SafeTensor&token=${TOKEN}" --content-disposition
+wget -O ./models/loras/mjmimic.safetensors "https://civitai.com/api/download/models/283697?type=Model&format=SafeTensor&token=${TOKEN}" --content-disposition
 wget -O ./models/controlnet/control_v11p_sd15_canny.safetensors https://huggingface.co/lllyasviel/control_v11p_sd15_canny/resolve/main/diffusion_pytorch_model.safetensors
