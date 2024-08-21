c30f23ed2d3601b79e13cb715f9046d44a49a14f
ross <ross@facere.ai>
2024-08-21 21:59:10 +1000

    add script for downloading models

--- ComfyUI-to-Python-Extension/test_img.py
+++ ComfyUI-to-Python-Extension/test_img.py
@@ -263,4 +263,4 @@ async def process_upload_image(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
