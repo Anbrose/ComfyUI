59daf0ab24780b48d0436b71418977fe21f19322
ross <ross@facere.ai>
2024-08-21 22:36:14 +1000

    add script for downloading models

--- ComfyUI-to-Python-Extension/test_img.py
+++ ComfyUI-to-Python-Extension/test_img.py
@@ -123,7 +123,7 @@ def import_custom_nodes() -> None:
     """
     import asyncio
     import execution
-    from nodes import init_custom_nodes
+    from nodes import init_builtin_extra_nodes
     import server
 
     # Creating a new event loop and setting it as the default loop
